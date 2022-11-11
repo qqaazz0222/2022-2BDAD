79ef8774cddb944f647092de4296473aabb165d8
qqaazz0222 <qqaazz0222@naver.com>
2022-11-11 18:42:27 +0900

    1111

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -4,9 +4,8 @@ const pool = require("../db/db");
 
 /* GET home page. */
 router.get("/", async (req, res, next) => {
-    const test1 = await pool.query("SELECT * FROM post");
-    console.log(test1[0]);
-    res.render("index", { title: "Node.js", test2: test1[0] });
+    const posts = await pool.query("SELECT * FROM post;");
+    res.render("index", { title: "Node.js", posts: posts[0] });
 });
 
 module.exports = router;
